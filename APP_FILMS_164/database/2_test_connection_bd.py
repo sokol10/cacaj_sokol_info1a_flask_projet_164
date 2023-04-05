c92eb5d819c69bef383b299b43b0c9a6c5dbe857
sokol.cacaj <sokol10@hotmail.fr>
2023-04-05 10:06:02 +0200

    1er commit

---
+++ APP_FILMS_164/database/2_test_connection_bd.py
@@ -0,0 +1,25 @@
+"""Démonstration d'envoi d'une requête SQL à la BD
+Fichier : 2_test_connection_bd.py
+Auteur : OM 2023.03.21
+"""
+
+from APP_FILMS_164.database.database_tools import DBconnection
+
+try:
+    """
+        Une seule requête pour montrer la récupération des données de la BD en MySql.
+    """
+    strsql_genres_afficher = """SELECT * FROM t_personne ORDER BY id_personne ASC"""
+
+    with DBconnection() as db:
+        db.execute(strsql_genres_afficher)
+        result = db.fetchall()
+        print("data_genres ", result, " Type : ", type(result))
+
+
+except Exception as erreur:
+    # print(f"2547821146 Connection à la BD Impossible ! {type(erreur)} args {erreur.args}")
+    print(f"2547821146 Test connection BD !"
+          f"{__name__,erreur} , "
+          f"{repr(erreur)}, "
+          f"{type(erreur)}")
